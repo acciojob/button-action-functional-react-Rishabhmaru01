a18db0400d13090a64368bb228d49aeeb592fcfa
Rishabhmaru01 <marurishu01@gmail.com>
2025-12-27 21:22:46 +0530

    updates

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
+
 function App() {
+
+    const [click, setClick] = useState(false)
+
   return (
     <div id="main">
-      // Do not alter the main div
+        <button id='click' onClick={()=>setClick(true)}>Click Me</button>
+
+        {click && (
+          <p id='para'>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+        )}
     </div>
   );
 }
